f74b8a0ecd72fac3ff1909a1ba11aeec7bdfe7f6
pll513 <pu_af@gzhc365.com>
2017-03-21 18:56:31 +0800

    event的滑动特效比较完善了

--- public/js/events.js
+++ public/js/events.js
@@ -40,14 +40,14 @@
   
   var ITEM_WIDTH = 356,
     THREE_ITEM_WIDTH = 1068,
-    sliderWrap = getElementsByClassName('slider__wrap')[0],
+    sliderWrap = d.getElementById('slider-wrap'),
     sliderItemList = sliderWrap.getElementsByTagName('div'),
     buttonWrap = d.getElementById('button-wrap'),
-    activeButton,
     buttonList = buttonWrap.getElementsByTagName('button'),
     arrowLeft = d.getElementById('arrow-left'),
     arrowRight = d.getElementById('arrow-right'),
-    map = {},
+    map = {}, // 形如 { 2017: -100, 2016: -80, 2015: -40, 2014: 0 }
+    positionArr = [], // 形如 [-0, -40, -80, -100]
     lenSliderItemList = sliderItemList.length,
     lenButtonList = buttonList.length,
     SLIDER_WIDTH = ITEM_WIDTH * lenSliderItemList,
@@ -56,14 +56,16 @@
     tmpId,
     lastJ = 0,
     lastYearEventCnt,
-    timerSlider;
+    timerSlider,
+    originIndex = 0,
+    isSliding = false;
   
   // 计算map 获得 按钮和位置 的映射
   for (i = 0; i < lenButtonList; ++i) {
     tmpId = buttonList[i].id;
     for (j = lastJ; j < lenSliderItemList; ++j) {
       if (~sliderItemList[j].className.indexOf(tmpId)) {
-        map[tmpId] = -ITEM_WIDTH * j;
+        positionArr[i] = map[tmpId] = -ITEM_WIDTH * j;
         lastJ = j;
         break;
       }
@@ -76,27 +78,27 @@
     SLIDER_WIDTH += (3 - lastYearEventCnt) * ITEM_WIDTH;
   }
   
-  console.log(SLIDER_WIDTH);
-  
   // 设置滑块容器宽度
   sliderWrap.style.width = SLIDER_WIDTH + 'px';
   
   // 点击左箭头向右滑一页
   addListener(arrowLeft, 'click', function () {
-    var oldLeft = parseFloat(sliderWrap.style.left) || 0,
-      destLeft = (Math.floor(oldLeft / THREE_ITEM_WIDTH) + 1) * THREE_ITEM_WIDTH;
-    destLeft = destLeft > 0 ? 0 : destLeft;
-    clearTimeout(timerSlider);
-    animateLeft(sliderWrap, destLeft);
+    if (isSliding) return;
+    var originLeft = parseFloat(sliderWrap.style.left) || 0,
+      // newLeft = (Math.floor(originLeft / THREE_ITEM_WIDTH) + 1) * THREE_ITEM_WIDTH;
+      newLeft = originLeft + THREE_ITEM_WIDTH;
+    newLeft = newLeft > 0 ? 0 : newLeft;
+    timerSlider = animateLeft(sliderWrap, newLeft);
   });
   
   // 点击右箭头向左滑一页
   addListener(arrowRight, 'click', function () {
-    var oldLeft = parseFloat(sliderWrap.style.left || 0),
-      destLeft = (Math.ceil(oldLeft / THREE_ITEM_WIDTH) - 1) * THREE_ITEM_WIDTH;
-    destLeft = destLeft < -SLIDER_WIDTH + THREE_ITEM_WIDTH ? -SLIDER_WIDTH + THREE_ITEM_WIDTH : destLeft;
-    clearTimeout(timerSlider);
-    animateLeft(sliderWrap, destLeft);
+    if (isSliding) return;
+    var originLeft = parseFloat(sliderWrap.style.left || 0),
+      // newLeft = (Math.ceil(originLeft / THREE_ITEM_WIDTH) - 1) * THREE_ITEM_WIDTH;
+      newLeft = originLeft - THREE_ITEM_WIDTH;
+    newLeft = newLeft < -SLIDER_WIDTH + THREE_ITEM_WIDTH ? -SLIDER_WIDTH + THREE_ITEM_WIDTH : newLeft;
+    timerSlider = animateLeft(sliderWrap, newLeft);
   });
   
   // 实践委托 点击指定年份滑动到对应年份的第一个
@@ -105,39 +107,67 @@
       target = event.target || event.srcElement;
     if (target.tagName === 'BUTTON') {
       clearTimeout(timerSlider);
-      activeButton = getElementsByClassName('active', buttonWrap)[0];
-      activeButton.className = "u-link";
-      target.className = "u-link active";
-      timerSlider = animateLeft(sliderWrap, map[target.id]);
+      animateLeft(sliderWrap, map[target.id]);
     }
   });
   
-  function animateLeft(ele, destLeft, duration) {
+  function positionToIndex(left) {
+    var i,
+      len = positionArr.length;
+    for (i = 0; i < len; ++i) {
+      if (left > positionArr[i]) {
+        return i - 1
+      }
+    }
+    return i - 1;
+  }
+  
+  /**
+   * 根据滑块当前位置和新位置切换当前标签
+   * @param newLeft {number}
+   */
+  function switchTag(newLeft) {
+    var newIndex = positionToIndex(newLeft);
+    
+    if (originIndex != newIndex) {
+      buttonList[originIndex].className = 'u-link';
+      buttonList[newIndex].className = 'u-link active';
+      originIndex = newIndex;
+    }
+    
+  }
+  
+  function animateLeft(ele, newLeft, duration) {
     var INTERVAL = 20,
-      oldLeft = parseFloat(ele.style.left) || 0,
+      originLeft = parseFloat(ele.style.left) || 0,
       moveCnt,
       step,
       move,
-      distance = destLeft - oldLeft;
+      distance = newLeft - originLeft;
+    
+    isSliding = true;
+    switchTag(newLeft);
     
     if (duration) {
       moveCnt = duration / INTERVAL;
-      step = (destLeft - oldLeft) / moveCnt;
+      step = (newLeft - originLeft) / moveCnt;
       if (distance < 0) {
         move = function () {
-          if (oldLeft + step < destLeft) {
-            ele.style.left = (oldLeft = destLeft) + 'px';
+          if (originLeft + step <= newLeft) {
+            ele.style.left = (originLeft = newLeft) + 'px';
+            isSliding = false;
           } else {
-            ele.style.left = (oldLeft += step) + 'px';
+            ele.style.left = (originLeft += step) + 'px';
             timerSlider = setTimeout(move, INTERVAL);
           }
         };
       } else {
         move = function () {
-          if (oldLeft + step > destLeft) {
-            ele.style.left = (oldLeft = destLeft) + 'px';
+          if (originLeft + step >= newLeft) {
+            ele.style.left = (originLeft = newLeft) + 'px';
+            isSliding = false;
           } else {
-            ele.style.left = (oldLeft += step) + 'px';
+            ele.style.left = (originLeft += step) + 'px';
             timerSlider = setTimeout(move, INTERVAL);
           }
         };
@@ -146,20 +176,22 @@
       if (distance < 0) {
         step = 30 * Math.floor(distance / THREE_ITEM_WIDTH);
         move = function () {
-          if (oldLeft + step < destLeft) {
-            ele.style.left = (oldLeft = destLeft) + 'px';
+          if (originLeft + step <= newLeft) {
+            ele.style.left = (originLeft = newLeft) + 'px';
+            isSliding = false;
           } else {
-            ele.style.left = (oldLeft += step) + 'px';
+            ele.style.left = (originLeft += step) + 'px';
             timerSlider = setTimeout(move, INTERVAL);
           }
         };
       } else {
         step = 30 * Math.ceil(distance / THREE_ITEM_WIDTH);
         move = function () {
-          if (oldLeft + step > destLeft) {
-            ele.style.left = (oldLeft = destLeft) + 'px';
+          if (originLeft + step >= newLeft) {
+            ele.style.left = (originLeft = newLeft) + 'px';
+            isSliding = false;
           } else {
-            ele.style.left = (oldLeft += step) + 'px';
+            ele.style.left = (originLeft += step) + 'px';
             timerSlider = setTimeout(move, INTERVAL);
           }
         };
@@ -168,47 +200,6 @@
     move();
   }
   
-  /**
-   * 选择指定类的标签
-   * @param className {string} 类名
-   * @param root [string, object] 根结点
-   * @param tag [string] 标签名
-   * @returns {Array}
-   */
-  function getElementsByClassName(className, root, tag) {
-    var elementList = [],
-      elements,
-      element,
-      i,
-      j,
-      lenElements,
-      lenClassList,
-      classList;
-    
-    root = typeof root === 'string' ? d.getElementById(root) : root;
-    if (!root) {
-      root = d.body;
-    }
-    
-    if (typeof root.getElementsByClassName === 'function') {
-      return root.getElementsByClassName(className);
-    }
-    
-    tag = tag || '*';
-    elements = root.getElementsByTagName(tag);
-    
-    for (i = 0, lenElements = elements.length; i < lenElements; ++i) {
-      element = elements[i];
-      classList = element.className.split(' ');
-      lenClassList = classList.length;
-      for (j = 0; j < lenClassList; ++j) {
-        if (classList[j] === className) {
-          elementList.push(element);
-          break;
-        }
-      }
-    }
-    return elementList;
-  }
-  
 })(window, document);
+
+//
